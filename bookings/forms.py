b7213202282d52d46a6e23c2e3aaa2620b1117c5
gustafsson96 <>
2023-09-25 20:07:31 +0000

    Add calendar datefield in forms.py

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,3 +6,17 @@ class ReservationForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['name', 'date', 'time', 'number_of_guests', 'message']
+        widgets = {
+            'date': forms.DateInput(
+                attrs={
+                    'type': 'date', 'placeholder': 'yyyy-mm-dd (DOB)', 'class': 'form-control'}
+            )}
+        labels = {
+            'name': ('Name '),
+            'date': ('Date '),
+            'time': ('Time '),
+            'number_of_guests': ('Number of Guests '),
+            'message': ('Special Requests or Allergy Information? Add here!'),
+        }
+
+form = ReservationForm()
