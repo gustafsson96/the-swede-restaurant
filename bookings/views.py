091d0af62870231ca343431f2df0011ee6de848d
gustafsson96 <>
2023-10-01 19:32:51 +0000

    Add 404.html file

--- bookings/views.py
+++ bookings/views.py
@@ -60,4 +60,7 @@ def delete_reservation(request, item_id):
 
 def contact(request):
     return render(request, 'contact.html', {})
-    
+
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
